58963df4af67a8ff87e7c7cbfd919ae5425c55a3
installer <installer@proletarsky.local>
2025-03-13 16:58:19 +0300

    Initial commit

---
+++ src/Templates/base.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Шаблон</title>
+	<link rel="stylesheet" href="css/style.css?v=2" type="text/css"> 
+</head>
+<body>
+<?php echo $content; ?>
+</body>
+</html>
