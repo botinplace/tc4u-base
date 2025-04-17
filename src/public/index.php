9642b38e9d60aa27982f8cf82222b76bbd5ba357
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-17 15:57:28 +0300

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -49,8 +49,15 @@ define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . '' . DIRECTORY_SEPARATOR);
 define('TEMPLATES', APP . 'Templates' . DIRECTORY_SEPARATOR);
 define('SITE', ROOT . 'public' . DIRECTORY_SEPARATOR);
+
+define('ROOT_DIR', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+//define('APP_DIR', ROOT_DIR . '/app'. DIRECTORY_SEPARATOR);
+define('APP_DIR', ROOT_DIR);
+define('PUBLIC_DIR', __DIR__);
+define('CONFIG_DIR', APP_DIR . '/Config'. DIRECTORY_SEPARATOR);
 define('PROJECT_PREFIX', '');
 define('BASE_URL', '');
+
 
 // Подключение автозагрузчика Composer
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -58,6 +65,18 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 // Инициализация приложения
 use Core\Application;
 use Core\ErrorLogger;
+use Core\Config\Config;
+use Core\Config\LoadEnv;
+
+if (!function_exists('env')) {
+    function env(string $key, $default = null) {
+        return $_ENV[$key] ?? $default;
+    }
+}
+
+// Загрузка переменных окружения
+Core\Config\LoadEnv::load('../.env');
+Config::load();
 
 $logger = new ErrorLogger(ROOT.'Logs/log', 1000);
 $app = new Application();
