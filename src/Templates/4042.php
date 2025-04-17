37a803ee8e2a95eeb0d3e69efd890e1bedd5c08f
botinplace <49042010+botinplace@users.noreply.github.com>
2025-04-17 16:13:05 +0300

    Update 4042.php

--- src/Templates/4042.php
+++ src/Templates/4042.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<meta http-equiv="X-UA-Compatible" content="IE=8" />
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />	
 	<title>Шаблон</title>
 	<link rel="stylesheet" href="css/style.css?v=2" type="text/css"> 
 </head>
